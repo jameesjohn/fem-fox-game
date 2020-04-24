61b9d94e453dd020c5acc4363439671c492c18cc
James James <jamesjay4199@gmail.com>
2020-04-24 21:20:41 +0100

    init and styling

---
+++ src/init.js
@@ -0,0 +1,24 @@
+import game from "./gamestate";
+
+const TICK_RATE = 3000;
+
+async function init() {
+  console.log("starting game");
+
+  let nextTimeToTick = Date.now();
+
+  function nextAnimationFrame() {
+    const now = Date.now();
+
+    if (nextTimeToTick <= now) {
+      game.tick();
+      nextTimeToTick = now + TICK_RATE;
+    }
+
+    requestAnimationFrame(nextAnimationFrame);
+  }
+
+  requestAnimationFrame(nextAnimationFrame);
+}
+
+init();
